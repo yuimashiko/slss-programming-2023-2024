70630066f2f217d79fd17a639d3006f2cf036dbd
Yui Mashiko <mashikokiwi121@gmail.com>
2023-09-21 14:47:21 -0700

    added new file

---
+++ quotegenerator.py
@@ -0,0 +1,24 @@
+# Quote Generator
+# Author: Ubial
+# Date: 19 September 2023
+
+# Output a quote from someone famous
+print("Do or do not. There is no try. --Yoda")
+
+# Print out another quote
+print("Git is Google drive on caffeine. --Ubial, probably")
+
+# Print out another, different quote
+print("Use the force, Luke. --Obi Wan Kenobi")
+
+# Ask the user a question
+input("What was your favourite quote? ")
+
+# Respond with something human would say
+print("Oh, that's my favourite, too.")
+
+# Ask the user what their favourite food is
+fave_food = input("What's your favourite food? ")
+
+# Respond to their answer
+print(f"Oooo, {fave_food} sounds good.")
