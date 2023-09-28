028ab73c16dd2ed521f8f1700f9b3c9e9635e446
Yui Mashiko <mashikokiwi121@gmail.com>
2023-09-28 13:56:39 -0700

    added conditionals file

--- Notes/Headers.md
+++
@@ -1,5 +0,0 @@
-The first three lines of code in our files are:
-
-> # Title of our code
-> # Author
-> # Date
---
+++ Notes/yui_chatbot.py
@@ -0,0 +1,9 @@
+# chat-bot
+# YUI
+# Sep 25
+
+print("Hi! I am chat-bot ver.3")
+
+input("What is your favorite food?")
+
+print("Oh really? I like it too!")
