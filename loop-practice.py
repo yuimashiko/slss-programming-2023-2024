d31c51a089f29f01b9816648cf0196c91b8e42a1
yuimashiko <144731052+yuimashiko@users.noreply.github.com>
2023-10-12 14:14:13 -0700

    Add files via upload
    Finished a new year countdown.

---
+++ loop-practice.py
@@ -0,0 +1,18 @@
+# NEW YEAR count down
+# YUI MASHIKO
+# oct/12
+import time
+
+# create a new year countdown
+print("We only have 10 seconds until 2024!")
+
+# start of at 10
+for i in range (10, 0, -1) :
+    print(i)
+    time.sleep(1)
+
+
+print("Happy new year!")
+
+
+  
