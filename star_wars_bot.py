96d24f53a5fdcc5239f7312743a3e0069aa4f4d7
yuimashiko <144731052+yuimashiko@users.noreply.github.com>
2023-10-17 10:07:26 -0700

    Add files via upload
    finished star_wars chat bot

---
+++ star_wars_bot.py
@@ -0,0 +1,25 @@
+# Star_wars_bot
+# YUI MASHIKO
+# Oct / 16
+
+import time
+
+print ("I will decide if you can join the dark side")
+
+fav_colour = input ("Is red your favourite colour?")
+
+
+if fav_colour.lower().strip(",. ?!") == ("yes") : 
+    print ("Dark side it is")
+
+else : 
+    cape = input ("Do you like capes?")
+    
+    if cape.lower().strip(",. ?!") == ("yes") : 
+        print ("Dark side it is")
+    
+    else : 
+        print ("You are on the light side!")
+ 
+    
+
